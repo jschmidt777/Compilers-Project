36d69ea0f6781c87278abccfd450ed9746cb72b0
jschmidt777 <schmidtjoseph39@gmail.com>
2016-02-11 20:44:51 -0500

    Checking for each kind of token
    Got rid of some things, and will go through in the order specified in
    class to identify tokens. Also, I will add functionality to remove white
    space and empty elements from the soucreCodeArray so the checks are
    cleaner.

--- Project/scripts/lexer2.js
+++ Project/scripts/lexer2.js
@@ -1,73 +1,63 @@
 /* lexer.js  */
  
-    //Initialize states for a link list of each kind of token
-    /*
-    var state0 = new createState0(); //start point of state transition diagram
-    var state1 = new createState(1, 4); // accepts id
-    var state2 = new createState(2, null); // accepts symbol (might need more for the individual symbols)
-    var state3 = new createState(3, null); // accepts digit
-    var state4 = new createState(4, null); // accepts keyword
-    */
+    
     //Globals
     var currentLineNum = 0;  //TODO: Figure out this functionality
 	var tokenStream = [];
     var insideString = false;
-    //var currentState = state0.state;
     
-/*
-    var stateMatrix = [ //  0-9
-                           [1], //0
-                           [acceptToken] //1
-
-
-
-
-
-
-                      ]*/
     //while, if, print, int, string, boolean, false, true
    
-    // Definitions for kinds of tokens so they can be compared 
-    // with the src code, which will then be added to the token stream. 
     
-    var keyword = /(while|if|print|int|string|boolean|false|true)/;
-	var identifier = /([a-z])/; 
-    var digit = /(\d)/;
-    var symbol = /(("[a-z]*")|{|}|\$|\"|==|!=|=|\+|(\s))/; //Capture any white space or a symbol, including strings
-    //var matchToken = "/" + identifier + "|" + digit + "|" + symbol + "/gm";
-
-
+    
+    
+    //"[a-z ]*" like this idea, but can't use it since a string is not a token. 
+    //TODO: Make count of quotation marks and test if there is an even amount of them.
+   
     function lex()
     {
         // Grab the "raw" source code.
         var sourceCode = document.getElementById("taSourceCode").value;
         // Trim the leading and trailing spaces.
-        sourceCode = trim(sourceCode);
+        sourceCode = sourceCode.trim();
+
+        // Definitions for kinds of tokens so they can be compared 
+        // with the src code, which will then be added to the token stream. 
+        var keyword = /(while|if|print|int|string|boolean|false|true)/gm;
+        var character = /("[a-z]*")/gm; //May not need this
+        var identifier = /([a-z])/gm; 
+        var digit = /(\d)/gm;
+        var symbol = /(\(|\)|{|}|\$|\"|==|!=|=|\+)/gm; //Capture any white space or a symbol, including strings
         
+        var splitOnVals = /(while|if|print|int|string|boolean|false|true|\(|\)|{|}|\$|\"|==|!=|=|\+|\s)/;
 
         //Array to go through the source code to split it into possible tokens. Pass 1.
         //Split on any white space or symbol and add them all to an array
-        var sourceCodeArray = sourceCode.split(symbol);
+        //Split at any keywords as well
+        var sourceCodeArray = sourceCode.split(splitOnVals);
         var isError = false;
         var currentLexeme = ""; //Look at each potential token
 
         for (i = 0; i < sourceCodeArray.length; i++){
             currentLexeme = sourceCodeArray[i];
             var nextLexeme = sourceCodeArray[i]+1;
-            var matchToken = /((while|if|print|int|string|boolean|false|true)|([a-z])|(\d)|( |{|}|\$|\"|==|!=|=|\+))/gm;
+            //var matchToken = /((while|if|print|int|string|boolean|false|true)|([a-z])|(\d)|({|}|\$|\"|==|!=|=|\+))/gm;
             
             //breakOn/splitOn array
             //new array that doesn't take whitespace
             //look at white space!
             //add name to kind (not too important)
             //3.7 crafting
-            if(matchToken.test(currentLexeme) /*&& identifier.test(nextChar)*/){
-                //while there isn't a match, add the next char to possibleKeyWord, and then check it until it finds the longest match.
-                createToken(currentLexeme, null, currentLineNum);
-                //possibleKeyWord =+ currentChar;
-                //checkLongestMatch(possibleKeyWord);
+            if(keyword.test(currentLexeme)){
+                createToken(currentLexeme, "keyword", currentLineNum);
+            }else if (identifier.test(currentLexeme)){
+                createToken(currentLexeme, "identifier", currentLineNum);
+            }else if (digit.test(currentLexeme)){
+                createToken(currentLexeme, "digit", currentLineNum);
+            }else if (symbol.test(currentLexeme)){
+                createToken(currentLexeme, "symbol", currentLineNum);
             }else{
-                //makeError(currentLineNum, " Invalid or unexpected token.");
+                makeError(currentLineNum, " Invalid or unexpected token.");
                 isError = true;
             }
         }
@@ -75,18 +65,17 @@
         //TODO: error checks for EOF, strings, blocks
         //assignKind(tokenStream);
 
-        return sourceCodeArray;
-        /*
+        return tokenStream;
+        
         if(!isError){
         return tokenStream;
         }else{
-        tokenStream = 0;
+        tokenStream = "nothing";
         return tokenStream;    
-        
-          //The error message will be displayed depending on what error lex found.  
-        }*/
+        //The error message will be displayed depending on what error lex found.  
         // should return a stream of tokens
-    }   
+        }
+    } 
 
     //Makes an instance of a token to be added to the tokenStream
     function tokenStruct() {
@@ -94,27 +83,10 @@
         this.kind = ""; //TODO: modify this to be one of only a couple kinds of tokens.
         this.lineNum = -1;
         this.toString = function(){
-            return "[" + this.lexeme + "," + this.kind + ",line:" + this.lineNum + "]";
+            return "<" + this.lexeme + "," + this.kind + ",line:" + this.lineNum + ">";
         }
     }
 
-
-
-    function createState0() {
-        this.state = 0;
-        this.nextState0 = 1; //id  
-        this.nextState1 = 2; //symbol/op
-        this.nextState2 = 3; //digit
-    }
-
-    function createState(state, nextState) {
-        this.state = state;
-        this.nextState = nextState;
-    }
-
-    function goToNextState(){
-        state = state.nextState;
-    }
 
     function createToken(lexeme, kind, lineNum){
         var token = new tokenStruct();
@@ -125,5 +97,5 @@
     }
 
     function makeError(lineNum, message){
-        document.getElementById("taOutput").value = "Lex error on line " + lineNum + ":" + message;
+        document.getElementById("taOutput").value = "Lex error on line " + lineNum + ":" + message + "\n";
     }
