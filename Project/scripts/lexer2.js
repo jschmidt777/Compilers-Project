e40e0ff1cde58609fa7e35360ffe34f62cd5fc56
jschmidt777 <schmidtjoseph39@gmail.com>
2016-02-13 13:04:12 -0500

    Better solution for id and char
    Figured out how to use the previously defined "insideString" boolean to
    determine whether the token is an id or a char. Last two things: error
    and warning handling, and line num...

--- Project/scripts/lexer2.js
+++ Project/scripts/lexer2.js
@@ -4,8 +4,7 @@
     //Globals
     var currentLineNum = 0;  //TODO: Figure out this functionality
 	var tokenStream = [];
-    var insideString = false;
-   
+    
     function lex()
     {
         // Grab the "raw" source code.
@@ -16,14 +15,14 @@
         // Definitions for kinds of tokens so they can be compared 
         // with the src code, which will then be added to the token stream. 
         var keyword = /(while|if|print|int|string|boolean|false|true)/;
-        var characterList = /("[a-z ]*")/; 
+        var characterList = /("[a-z ]*")/; //May not need this
         var identifier = /([a-z])/; 
         var digit = /(\d)/;
         var symbol = /(\(|\)|{|}|\$|\"|==|!=|=|\+)/; 
 
         //Split on any white space, deliminators, and even id, char, and digits. 
         //Basically, split on anything we may find interesting, since that will be tokenized.
-        var splitOnVals = /(while|if|print|int|string|boolean|false|true|"[a-z ]*"|[a-z]|[0-9]|\"|\(|\)|{|}|\$|==|!=|=|\+|\s|)/;
+        var splitOnVals = /(while|if|print|int|string|boolean|false|true|[a-z]|[0-9]|\"|\(|\)|{|}|\$|==|!=|=|\+|\s|)/;
                             //Keywords                                     id/chars/digits              deliminators
 
         //Array to go through the source code to split it into possible tokens. Pass 1.
@@ -31,35 +30,46 @@
         //Get rid of white space in the sourceCodeArray, as well as any empty array elements
         sourceCodeArray = sourceCodeArray.filter(function(lexeme){ return lexeme.replace(/(\r\n|\n|\r|\s)/gm,"")}); //reference: http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript?rq=1
         var isError = false;
-        var quotesCount = 0;
+        var insideString = false;
 
         for (i = 0; i < sourceCodeArray.length; i++){
             var currentLexeme = sourceCodeArray[i]; //Look at each potential token   
             var nextLexeme = sourceCodeArray[i]+1;        
             if(keyword.test(currentLexeme)){
                 createToken(currentLexeme, "keyword", currentLineNum);
-            }else if (characterList.test(currentLexeme)){
-                createToken(currentLexeme, "characterList", currentLineNum);    
-            }else if (identifier.test(currentLexeme)){
+            }else if (identifier.test(currentLexeme) && !insideString){
                 createToken(currentLexeme, "identifier", currentLineNum);
+            }else if (identifier.test(currentLexeme) && insideString){
+                createToken(currentLexeme, "char", currentLineNum);
             }else if (digit.test(currentLexeme)){
                 createToken(currentLexeme, "digit", currentLineNum);
             }else if (symbol.test(currentLexeme)){
                 switch(currentLexeme){
                     case "{":
                         createToken(currentLexeme, "openBlock", currentLineNum);
+                        //count for these?
                     break;
                     case "}":
                         createToken(currentLexeme, "closeBlock", currentLineNum);
+                        //count for these?
                     break;
                     case "(":
                         createToken(currentLexeme, "openParen", currentLineNum);
+                        //count for these?
                     break;
                     case ")":
                         createToken(currentLexeme, "closeParen", currentLineNum);
+                        //count for these?
                     break;
                     case "\"": //This will need to be based on the quotation count
-                        createToken(currentLexeme, "quotation", currentLineNum);
+                        if(!insideString){
+                            createToken(currentLexeme, "openQuotation", currentLineNum);
+                            insideString = true;
+                        }else if(insideString){
+                            createToken(currentLexeme, "closeQuotation", currentLineNum);
+                            insideString = false;
+                        }
+                
                     /*  May not even need this
                         if(quotesCount == 0){
                             createToken(currentLexeme, "openQuotation", currentLineNum);
@@ -89,15 +99,19 @@
                     break;
                     default:  createToken(currentLexeme, "symbol", currentLineNum);
                 }
+            
             }else{
                 makeError(currentLineNum, " Invalid or unexpected token.");
                 isError = true;
-            }
+               
         }
+    }
         //TODO: error checks for EOF, strings
         //if there was never an assigned EOF, find the last assigned } and add a $, plus a warning saying it was added
-        //if quoteCount is greater than one, throw an error
-
+        if(insideString){
+                    makeError(currentLineNum, " Unclosed quotation.");
+                    isError = true;
+                }
         if(!isError){
         return tokenStream;
         }else{
